5e2bfcf506bfb0ad06827d19fc77f74f14c63edb
burakturksever <47771419+burakturksever@users.noreply.github.com>
2020-06-13 20:59:26 +0300

    Commit

--- PeoplePerson/app/src/main/java/com/aytugburak/peopleperson/classes/Contact.java
+++ PeoplePerson/app/src/main/java/com/aytugburak/peopleperson/classes/Contact.java
@@ -1,9 +1,71 @@
 package com.aytugburak.peopleperson.classes;
 
 public class Contact {
-    private int id;
     private String name;
     private String surname;
     private String birthDate;
     private String category;
+    private boolean favorited;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isFavorited() {
+        return favorited;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setFavorited(boolean favorited) {
+        this.favorited = favorited;
+    }
+
+    public Contact(String name, String surname, String birthDate, String category, boolean favorited) {
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.category = category;
+        this.favorited = favorited;
+    }
+
+    // for debugging, getters and setters will be used mostly
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", category='" + category + '\'' +
+                ", favorited=" + favorited +
+                '}';
+    }
 }
+
---
+++ PeoplePerson/app/src/main/java/com/aytugburak/peopleperson/classes/RecyclerFragment.java
@@ -0,0 +1,31 @@
+package com.aytugburak.peopleperson.classes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aytugburak.peopleperson.R;
+
+public class RecyclerFragment extends Fragment {
+
+    public RecyclerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recycler, container, false);
+    }
+}
