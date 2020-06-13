343ebd57cbb0a0090f5f2dc52ebaacdb4c6a2da6
burakturksever <47771419+burakturksever@users.noreply.github.com>
2020-06-13 20:16:16 +0300

    Initial for FIles

---
+++ PeoplePerson/app/src/main/java/com/aytugburak/peopleperson/classes/Contact.java
@@ -0,0 +1,9 @@
+package com.aytugburak.peopleperson.classes;
+
+public class Contact {
+    private int id;
+    private String name;
+    private String surname;
+    private String birthDate;
+    private String category;
+}
